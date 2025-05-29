d9e4a509cf9b1f362629f90db4a983af9e880896
Vladyslav Petrenko <legionvlad@icloud.com>
2025-05-29 15:07:40 +0200

    test

--- models/booking.js
+++ models/booking.js
@@ -1,14 +1,29 @@
 const mongoose = require('mongoose');
 
 const bookingSchema = new mongoose.Schema({
-  email: { type: String, required: true },
-  name: String,
-  phone: String,
-  from: String,
-  to: String,
-  date: Date,
-  confirmed: { type: Boolean, default: false },
-  // другие поля по необходимости
-}, { timestamps: true });
+  locale: { type: String, default: "ru" },
+   from: String,
+   to: String,
+   date: Date,
+   returnDate: Date,
+   adults: Number,
+   children: Number,
+   name: String,
+   phone: String,
+   email: String,
+   baggage: String,
+   comment: String,
+   isRoundTrip: Boolean,
+   price: Number,
+   duration: Number,
+   id: String,
+   tripPurpose: String,
+   garant: Boolean,
+   confirmed: { type: Boolean, default: false },
+   createdAt: { type: Date, default: Date.now },
+ });
+ 
+ const Booking1 = mongoose.model("Booking1", bookingSchema1, "bookings");
+
 
 module.exports = mongoose.model('Booking', bookingSchema);
