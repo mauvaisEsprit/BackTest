39e8d6ce367a03595df72653590f41825cd410ae
Vladyslav Petrenko <legionvlad@icloud.com>
2025-05-29 15:10:55 +0200

    test

--- models/booking.js
+++ models/booking.js
@@ -1,29 +1,29 @@
 const mongoose = require('mongoose');
 
-const bookingSchema = new mongoose.Schema({
+const bookingSchema1 = new mongoose.Schema({
   locale: { type: String, default: "ru" },
-   from: String,
-   to: String,
-   date: Date,
-   returnDate: Date,
-   adults: Number,
-   children: Number,
-   name: String,
-   phone: String,
-   email: String,
-   baggage: String,
-   comment: String,
-   isRoundTrip: Boolean,
-   price: Number,
-   duration: Number,
-   id: String,
-   tripPurpose: String,
-   garant: Boolean,
-   confirmed: { type: Boolean, default: false },
-   createdAt: { type: Date, default: Date.now },
- });
- 
- const Booking1 = mongoose.model("Booking1", bookingSchema1, "bookings");
+  from: String,
+  to: String,
+  date: Date,
+  returnDate: Date,
+  adults: Number,
+  children: Number,
+  name: String,
+  phone: String,
+  email: String,
+  baggage: String,
+  comment: String,
+  isRoundTrip: Boolean,
+  price: Number,
+  duration: Number,
+  id: String,
+  tripPurpose: String,
+  garant: Boolean,
+  confirmed: { type: Boolean, default: false },
+  createdAt: { type: Date, default: Date.now },
+});
 
+// Создаём модель с именем "Booking1" и явно указываем коллекцию "bookings"
+const Booking = mongoose.model("Booking", bookingSchema1, "bookings");
 
-module.exports = mongoose.model('Booking', bookingSchema);
+module.exports = Booking;
