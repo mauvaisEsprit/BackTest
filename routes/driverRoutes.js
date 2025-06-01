080834199982dd07ccdde7f03fe6892ab190e969
Vladyslav Petrenko <legionvlad@icloud.com>
2025-06-01 21:56:41 +0200

    test

--- routes/driverRoutes.js
+++ routes/driverRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const driverController = require('../controllers/driverController');
 const auth = require('../middleware/isDriver');
 
-router.get('/', auth, driverController.getProfile);
+router.get('/profile', auth, driverController.getProfile);
 router.put('/update', auth, driverController.updateProfile);
 router.post('/change-password',auth, driverController.changePassword);
 
